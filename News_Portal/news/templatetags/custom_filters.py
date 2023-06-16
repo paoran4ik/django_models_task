7eda07798d82187caa2ae15ddcb1e13b6df94fb3
paoran4ik <orlov.pasha.99@gmail.com>
2023-06-16 05:36:38 +0300

    new task

--- News_Portal/news/templatetags/custom_filters.py
+++ News_Portal/news/templatetags/custom_filters.py
@@ -3,13 +3,7 @@ from django import template
 register = template.Library()
 
 
-# Регистрируем наш фильтр под именем currency, чтоб Django понимал,
-# что это именно фильтр для шаблонов, а не простая функция.
 @register.filter()
 def censor(value):
     value = str(value).replace("редиска", "*******")
-
-
-
-    # Возвращаемое функцией значение подставится в шаблон.
     return f'{value}'
